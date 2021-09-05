6d742d3461942690b03bbb07040a6f34ee9ebdb2
Alp Avanoglu <alpavanoglu@Alps-iMac.fritz.box>
2021-09-05 22:01:47 +0200

    Add  to

--- Sources/Queue/Queue.swift
+++ Sources/Queue/Queue.swift
@@ -1,2 +1,19 @@
-public struct Queue {
+public struct Queue<Value: Equatable> {
+    private var linkedList: LinkedList<Value> = []
+    
+    public var front: Value? {
+        return linkedList.tail
+    }
+    
+    public var rear: Value? {
+        return linkedList.head
+    }
+    
+    public mutating func enqueue(value: Value) {
+        linkedList.append(value)
+    }
+    
+//    public func dequeue() -> Value {
+//        
+//    }
 }
--- Tests/QueueTests/QueueTests.swift
+++ Tests/QueueTests/QueueTests.swift
@@ -2,4 +2,15 @@ import XCTest
 @testable import Queue
 
 final class QueueTests: XCTestCase {
+    
+    // MARK: - `enqueue:value`
+    
+    func testEnqueueAddsToTheBeginningOfTheList() {
+        var queue = Queue<Int>()
+        queue.enqueue(value: 1)
+        queue.enqueue(value: 2)
+        queue.enqueue(value: 3)
+        XCTAssertEqual(queue.front, 1)
+    }
+    
 }
